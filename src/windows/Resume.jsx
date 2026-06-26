6ed333109c76aa28de481e34edd7dcf614fc982f
AshmitSherigar <sherigarashmit@gmail.com>
2026-06-27 00:52:28 +0530

    added resume window and also part of safari

---
+++ src/windows/Resume.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { WindowControls } from '../components';
+import { Download } from 'lucide-react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import WindowWrapper from '../hoc/WindowWrapper';
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
+const Resume = () => {
+  return (
+    <>
+      <div id="window-header">
+        <WindowControls target={'resume'} />
+        <h2>Resume.pdf</h2>
+        <a
+          href="files/resume.pdf"
+          download={true}
+          className="cursor-pointer"
+          title="Download resume"
+        >
+          <Download className="icon" />
+        </a>
+      </div>
+      <Document file="files/resume.pdf">
+        <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
+      </Document>
+    </>
+  );
+};
+const ResumeWindow = WindowWrapper(Resume, 'resume');
+export default ResumeWindow;
